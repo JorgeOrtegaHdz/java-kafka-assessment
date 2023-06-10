ade0dca04d53c721a382240c4cb694645cbc17b3
Jorge Ortega <Jorge.Ortega@Honeywell.com>
2023-06-10 02:56:35 -0600

    (dev) fix the save in database

--- src/main/java/com/example/interview/entity/Scan.java
+++ src/main/java/com/example/interview/entity/Scan.java
@@ -1,12 +1,13 @@
 package com.example.interview.entity;
 
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-
 import java.time.LocalDateTime;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 
 @Entity
 public class Scan {
--- src/main/java/com/example/interview/repository/ScanRepository.java
+++ src/main/java/com/example/interview/repository/ScanRepository.java
@@ -1,8 +1,10 @@
 package com.example.interview.repository;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
 import com.example.interview.entity.Scan;
 
+@Repository
 public interface ScanRepository extends CrudRepository<Scan, Integer> {
-
 }
